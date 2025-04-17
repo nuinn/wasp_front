8ee17342acc28d7af3f704918398a1dff5724d7c
nuinn <dilley.marc@gmail.com>
2025-04-17 14:25:54 +0200

    wasp boilerplate

---
+++ src/components/createLog.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, shallowRef } from "vue";
+
+const currentStep = ref(0)
+
+const steps = shallowRef([])
+
+function next() {
+  if (currentStep.value < steps.value.length) currentStep.value++
+}
+
+function back() {
+  if (currentStep.value) currentStep.value--
+}
+
+</script>
+
+<template>
+  <component :is="steps[currentStep]"></component>
+
+  <div class="nav-buttons">
+
+  </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
